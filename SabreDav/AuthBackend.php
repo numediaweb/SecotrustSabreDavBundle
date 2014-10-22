083dcbb201bb6a2cc3aa9ced9bf344f39513d29c
lduer <duerlukas@gmail.com>
2014-10-22 16:03:46 +0200

    updated auth-backend

--- SabreDav/AuthBackend.php
+++ SabreDav/AuthBackend.php
@@ -14,7 +14,9 @@ namespace Secotrust\Bundle\SabreDavBundle\SabreDav;
 use Sabre\DAV\Auth\Backend\BackendInterface;
 use Sabre\DAV\Exception;
 use Sabre\DAV\Server;
+use Sabre\HTTP;
 use Symfony\Component\Security\Core\SecurityContextInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class AuthBackend implements BackendInterface
 {
@@ -24,32 +26,114 @@ class AuthBackend implements BackendInterface
     private $context;
 
     /**
+     * @var ContainerInterface
+     */
+    private $container;
+    
+    /**
+     * @var type 
+     */
+    private $currentUser;
+    
+    /**
      * Constructor
      *
      * @param SecurityContextInterface $context
      */
-    public function __construct(SecurityContextInterface $context)
+    public function __construct(SecurityContextInterface $context, ContainerInterface $container)
     {
         $this->context = $context;
+        $this->container = $container;
     }
-
+    
     /**
-     * @inheritdoc
+     * Checks if username and password are valid. (Checked by the FOSUserManager)
+     * Returns 
+     * 
+     * @param type $username
+     * @param type $password
+     * @return boolean
+     */
+    public function validateUserPass($username, $password) 
+    {
+
+        $userManager = $this->container->get('fos_user.user_manager');
+        $user = $userManager->findUserByUsername($username);
+
+        if (is_null($user)) {
+            return false;
+        }
+        $encoder_service = $this->container->get('security.encoder_factory');
+        $encoder = $encoder_service->getEncoder($user);
+        $encoded_pass = $encoder->encodePassword($password, $user->getSalt());
+
+        if ($encoded_pass === $user->getPassword()) {
+            return true;
+        } 
+
+        return false;
+    }
+    
+    /**
+     * Authenticate
+     * 
+     * Authenticates the User with the HTTP\BasicAuth()
+     * if the user is not logged in via Browser in the Tool.
+     * 
+     * 
+     * @param Server $server
+     * @param type $realm
+     * @return void
      */
     public function authenticate(Server $server, $realm)
     {
         if (null === $this->context->getToken()) {
-            throw new Exception('The security token is NULL');
+            throw new Exception\NotAuthenticated('The security token is NULL');
         }
 
-        return $this->context->getToken()->isAuthenticated();
+        $auth = new HTTP\BasicAuth();
+        $auth->setHTTPRequest($server->httpRequest);
+        $auth->setHTTPResponse($server->httpResponse);
+        $userpass = $auth->getUserPass();
+
+        if (!$userpass) {
+            $auth->requireLogin();
+            throw new Exception\NotAuthenticated('No authentication headers were found');
+        }
+
+        // Authenticates the user
+        if (!$this->validateUserPass($userpass[0],$userpass[1])) {
+            $auth->requireLogin();
+            throw new Exception\NotAuthenticated('Username or password does not match');
+        }
+
+        $this->currentUser = $userpass[0];
+
+        return true;
     }
 
+    /**
+     * Save User-Login to session
+     * 
+     * @param type $userpass
+     */    
+    private function userLoginAction($userpass)
+    {
+        //process Symfony2 Login
+        $userManager = $this->container->get('fos_user.user_manager');
+        $user = $userManager->findUserByUsername($userpass[0]);
+
+        $token = new \Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken($user, $user->getPassword(), 'main', $user->getRoles());
+        $request = $this->container->get('request');
+        $session = $request->getSession();
+        $session->set('_security_main',  serialize($token));        
+    }
+    
     /**
      * @inheritdoc
      */
     public function getCurrentUser()
     {
-        return $this->context->getToken()->getUsername();
+        return $this->currentUser;
     }
 }
